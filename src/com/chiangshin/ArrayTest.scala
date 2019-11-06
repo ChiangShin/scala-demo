c0b0ab672796dc5f24e2cb75593f327a182fa111
ChiangShin <jiangxin792@sina.com>
2019-11-06 23:17:43 +0800

    111

---
+++ src/com/chiangshin/ArrayTest.scala
@@ -0,0 +1,10 @@
+package com.chiangshin
+
+import java.util.Arrays
+
+
+object ArrayTest {
+  def main(args: Array[String]): Unit = {
+      Arrays.asList("ff","ffsf")
+  }
+}
---
+++ src/com/chiangshin/StringTest.scala
@@ -0,0 +1,21 @@
+package com.chiangshin
+
+/**
+  * 字符串练习
+  */
+object StringTest {
+
+  def main(args: Array[String]): Unit = {
+    var str1 = "10000"
+    str1 += 'f'
+    println(str1)
+
+    val str2 = str1.concat("v")
+    println(str2)
+
+
+
+
+
+  }
+}
